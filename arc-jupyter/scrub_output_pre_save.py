cce8349595ddeb1a7beb16888873621aab30a1f3
Mike Seddon <seddonm1@gmail.com>
2019-10-17 17:12:37 +1100

    Update drivers, deltalake and kafka plugins

--- arc-jupyter/scrub_output_pre_save.py
+++ arc-jupyter/scrub_output_pre_save.py
@@ -4,7 +4,7 @@ def scrub_output_pre_save(model, **kwargs):
     # only run on notebooks
     if model['type'] != 'notebook':
         return
-        
+
     # only run on nbformat v4
     if model['content']['nbformat'] != 4:
         return
@@ -22,4 +22,3 @@ def scrub_output_pre_save(model, **kwargs):
 
 
 c.FileContentsManager.pre_save_hook = scrub_output_pre_save
-
