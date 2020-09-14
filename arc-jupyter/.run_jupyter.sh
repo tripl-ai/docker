6dd9e5f0047c8b7d7ac0d2e5fdeba3f16206da3b
Mike Seddon <seddonm1@gmail.com>
2020-09-15 08:30:37 +1000

    change port

--- arc-jupyter/.run_jupyter.sh
+++ arc-jupyter/.run_jupyter.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo "arc: ${ARC_VERSION} arc-jupyter: ${ARC_JUPYTER_VERSION} spark: ${SPARK_VERSION} hadoop: ${HADOOP_VERSION}"
+echo ""
 /usr/local/bin/jupyter lab --allow-root --ip 0.0.0.0 --config=/home/jovyan/.jupyter/jupyter_notebook_config.py
