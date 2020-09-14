548b9a485f08f7557f8610c7fafdb7cfe17a76c0
Mike Seddon <seddonm1@gmail.com>
2020-09-15 09:34:53 +1000

    update exec file

--- arc-jupyter/.jupyter/jupyter_notebook_config.py
+++ arc-jupyter/.jupyter/jupyter_notebook_config.py
@@ -914,14 +914,12 @@ def scrub_output_pre_save(model, **kwargs):
         cell['execution_count'] = None
         cell['metadata'] = {}
 
-
 c.FileContentsManager.pre_save_hook = scrub_output_pre_save
 
 c.NotebookApp.token = ''
 c.NotebookApp.password = ''
 c.NotebookApp.open_browser = False
-c.NotebookApp.port = 8080
-c.NotebookApp.allow_origin_pat = '(^https://8080-dot-[0-9]+-dot-devshell\.appspot\.com$)|(^https://colab\.research\.google\.com$)|((https?://)?[0-9a-z]+-dot-datalab-vm[\-0-9a-z]*.googleusercontent.com)|((https?://)?[0-9a-z]+-dot-[\-0-9a-z]*.notebooks.googleusercontent.com)'
+c.NotebookApp.port = 8888
 c.NotebookApp.allow_remote_access = True
 c.NotebookApp.disable_check_xsrf = False
 c.NotebookApp.notebook_dir = '/home/jovyan'
--- arc-jupyter/.run_jupyter.sh
+++ arc-jupyter/.run_jupyter.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-echo "arc: ${ARC_VERSION} arc-jupyter: ${ARC_JUPYTER_VERSION} spark: ${SPARK_VERSION} hadoop: ${HADOOP_VERSION}"
-echo ""
+echo "
+    ___                     __                  __
+   /   |  __________       / /_  ______  __  __/ /____  _____
+  / /| | / ___/ ___/  __  / / / / / __ \/ / / / __/ _ \/ ___/
+ / ___ |/ /  / /__   / /_/ / /_/ / /_/ / /_/ / /_/  __/ /
+/_/  |_/_/   \___/   \____/\__,_/ .___/\__, /\__/\___/_/
+                               /_/    /____/
+
+arc: ${ARC_VERSION} arc-jupyter: ${ARC_JUPYTER_VERSION} spark: ${SPARK_VERSION} hadoop: ${HADOOP_VERSION}
+"
 /usr/local/bin/jupyter lab --allow-root --ip 0.0.0.0 --config=/home/jovyan/.jupyter/jupyter_notebook_config.py
