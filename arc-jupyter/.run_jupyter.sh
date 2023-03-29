965bada98b9c55550662fee5831c2c4e562e1a3b
John Bruce <john.a.m.bruce@gmail.com>
2023-03-29 19:09:25 -0400

    Update to Arc 4.1.0, Spark 3.3.2
    Update to Jupyter Lab 3.6.2
    Remove old delta lib

--- arc-jupyter/.run_jupyter.sh
+++ arc-jupyter/.run_jupyter.sh
@@ -9,4 +9,4 @@ echo "
 
 arc: ${ARC_VERSION} arc-jupyter: ${ARC_JUPYTER_VERSION} spark: ${SPARK_VERSION} hadoop: ${HADOOP_VERSION}
 "
-/usr/local/bin/jupyter lab --allow-root --ip 0.0.0.0 --config=/home/$NB_USER/.jupyter/jupyter_notebook_config.py
+/usr/local/bin/jupyter lab --allow-root --ip 0.0.0.0 --no-browser --config=/home/$NB_USER/.jupyter/jupyter_notebook_config.py
